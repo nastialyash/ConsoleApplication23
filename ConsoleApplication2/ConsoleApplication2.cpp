98a0ce879ddd7c4dd70a9e9e347a1740980cddef
1ALAR1 <senchuk.yuriy85@gmail.com>
2024-09-05 17:35:22 +0300

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,108 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+
+
+    int a;
+cout << "Enter your number : " << endl;
+cin >> a;
+if (a % 2==0) {
+    cout << "Num is good" << endl;
+
+}
+else {
+    cout << "Num is not good" << endl;
+}
+
+int a, b;
+cout << "Enter  num1 : " << endl;
+cin >> a;
+cout << "Enter num2 : " << endl;
+cin >> b;
+if (a > b) {
+    cout << "Num is least   " << b << endl;
+
+}
+else if(a<b){
+    cout << "Num is least" << a << endl;
+
+}
+else {
+    cout << "Num is simple" << endl;
+}
+
+int a;
+cout << "Enter your number : " << endl;
+cin >> a;
+if (a>0) {
+    cout << "This num is + :  "  << endl;
+}
+else if(a<0) {
+    cout << "This num is - : " << endl;
+
+
+}
+else {
+    cout << "Num is 0 : " << endl;
+}
+
+int a, b;
+cout << "Enter yout num1 : " << endl;
+cin >> a;
+cout << "Enter your num2"  << endl;
+cin >> b;
+if (a == b) {
+    cout << "Num is simple" << endl;
+}
+else {
+    if (a < b) {
+        cout << "All nums : " << a  << b << endl;
+
+}
+    else {
+        cout << "All nums : " << b  << a << endl;
+    }
+
+
+
+}
+
+
+int a, b, c, d,e;
+cout << "Enter yout num1 : " << endl;
+cin >> a;
+cout << "Enter yout num2 : " << endl;
+cin >> b;
+cout << "Enter yout num3 : " << endl;
+cin >> c;
+cout << "Enter yout num4 : " << endl;
+cin >> d;
+cout << "Enter yout num5 : " << endl;
+cin >> e;
+if (a, b, c, d > 4) {
+    cout << "Is good" << endl;
+}
+
+else if (a, b, c, d < 4) {
+    cout << "Is not good" << endl;
+}
+
+int a;
+cout << "Enter your number : " << endl;
+cin >> a;
+if (a % 2 == 0) {
+    a *=3;
+    cout << "Num is steamy " << a << endl;
+}
+else {
+    a /= 2;
+    cout << "Num is not steamy" << a << endl;
+}
+
+
+}
